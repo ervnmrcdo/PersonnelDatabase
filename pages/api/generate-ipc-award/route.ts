565a01e4a40bfe472ec2508506fb3601e1c3edbb
Cj <cjtb2005@gmail.com>
2026-02-10 01:37:32 +0800

    add: pending awards in admin dash

--- pages/api/generate-ipc-award/route.ts
+++ pages/api/generate-ipc-award/route.ts
@@ -130,7 +130,7 @@ export default async function POST(req: NextApiRequest, res: NextApiResponse) {
           ${nonTeachingId},
           1,
           ${buffer},
-          'Pending',
+          'PENDING',
           CURRENT_DATE
         )
         RETURNING *;
