4a38140dd47d3842553b4854df1713a14a0d085f
ervnmrcdo <ervinmercado31@gmail.com>
2026-02-18 12:39:26 +0800

    add: supabase create client

--- pages/api/default.ts
+++ pages/api/default.ts
@@ -1,12 +1,23 @@
-import sql from "@/config/db";
+import { createPagesServerClient } from "@/lib/supabase/pager-server"
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function trial(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const data = await sql`SELECT * FROM  awards`;
+    // This helper specifically takes 'req' and 'res' to handle cookies correctly
+    const supabase = createPagesServerClient(req, res);
 
-    res.status(200).json(data);
+    const { data, error } = await supabase
+      .from('publications')
+      .select('*');
+
+    if (error) {
+      return res.status(400).json({ error: error.message });
+    }
+
+    return res.status(200).json(data);
   } catch (e) {
-    res.status(500).json("Internal Server Error");
+    // Casting e as Error to access the message safely
+    const errorMessage = e instanceof Error ? e.message : 'Unknown error';
+    return res.status(500).json({ error: `Server Error: ${errorMessage}` });
   }
 }
