b4043a8b46ec15d144195a32e0baec2a47a00623
ervnmrcdo <ervinmercado31@gmail.com>
2026-02-24 09:43:19 +0800

    fix: migrate route

--- pages/api/resubmit-award/route.ts
+++ pages/api/resubmit-award/route.ts
@@ -1,35 +1,48 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import sql from "@/config/db";
+import { createPagesServerClient } from "@/lib/supabase/pager-server";
 
 export default async function POST(req: NextApiRequest, res: NextApiResponse) {
   try {
     if (req.method !== "POST") {
       return res.status(405).send("Method Not Allowed");
     }
-    const data = JSON.parse(await req.body);
+    const payload = JSON.parse(await req.body);
     const {
       ipaData,
       buffer,
       submission_id,
       newLogs
-    } = data;
+    } = payload;
 
 
     const foo = Buffer.from(buffer)
-    const admin_id = 1;
+    const supabase = createPagesServerClient(req, res);
 
-    const result = await sql`
-     	UPDATE 
-          pendingawards
-     	SET 
-          attached_files = ${foo}, pdf_json_data = ${JSON.stringify(ipaData)}, status = 'PENDING',
-          reviewed_by_admin_id = ${admin_id}, logs = ${JSON.stringify(newLogs)}
-     	WHERE 
-          submission_id = ${submission_id} AND status = 'RETURNED'
-        RETURNING *;
-      `;
+    const { data, error } = await supabase
+      .from('submissions')
+      .update({
+        attached_files: foo,
+        pdf_json_data: JSON.stringify(ipaData),
+        status: 'PENDING',
+        logs: newLogs
+      })
+      .eq('submission_id', submission_id)
+      .eq('status', 'RETURNED')
+      .select()
 
-    return res.status(200).json(result);
+    // const result = await sql`
+    //  	UPDATE 
+    //       pendingawards
+    //  	SET 
+    //       attached_files = ${foo}, pdf_json_data = ${JSON.stringify(ipaData)}, status = 'PENDING',
+    //       reviewed_by_admin_id = ${admin_id}, logs = ${JSON.stringify(newLogs)}
+    //  	WHERE 
+    //       submission_id = ${submission_id} AND status = 'RETURNED'
+    //     RETURNING *;
+    //   `;
+
+    return res.status(200).json(data);
 
   } catch (err) {
     console.error(err);
